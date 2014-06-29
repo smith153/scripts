caa04c937593f0ed2179a56f3e094230036d6d6a
Samuel Smith <leon36@gmail.com>
2014-06-28 20:22:18 -0500

    adding perl_LWP_benchmarks

---
+++ perl_LWP_benchmarks/README.md
@@ -0,0 +1,29 @@
+Benchmarking Perl LWP Based Modules
+=======
+
+Results:
+
+Loading LWP::UserAgent
+Before 1000 iterations: Size in kbytes: 8.2744140625
+emily     9495 20.0  0.6  28180  5744 pts/10   S+   14:37   0:00 perl test1.pl
+After iterations: Size in kbytes: 8.3759765625
+emily     9495 72.2  1.2  49800 11176 pts/10   S+   14:37   0:10 perl test1.pl
+Ended after 14 seconds
+Loading LWP::Curl
+Before 1000 iterations: Size in kbytes: 0.70703125
+emily     9505  0.0  0.6  74420  5564 pts/10   S+   14:38   0:00 perl test1.pl
+After iterations: Size in kbytes: 0.70703125
+emily     9505 17.6  0.6  78580  6096 pts/10   S+   14:38   0:00 perl test1.pl
+Ended after 5 seconds
+Loading WWW::Mechanize
+Before 1000 iterations: Size in kbytes: 13.6337890625
+emily     9514 65.0  1.3  50884 12004 pts/10   S+   14:38   0:00 perl test1.pl
+After iterations: Size in kbytes: 4083.9208984375
+emily     9514 80.9  2.1  61712 19128 pts/10   S+   14:38   0:20 perl test1.pl
+Ended after 25 seconds
+Loading HTTP::Tiny
+Before 1000 iterations: Size in kbytes: 0.412109375
+emily     9525  0.0  0.4  26992  4488 pts/10   S+   14:38   0:00 perl test1.pl
+After iterations: Size in kbytes: 0.5068359375
+emily     9525 60.0  0.5  29220  4888 pts/10   S+   14:38   0:04 perl test1.pl
+Ended after 8 seconds
---
+++ perl_LWP_benchmarks/test_lwp.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/env perl 
+use warnings;
+use strict;
+use Getopt::Long;
+use Devel::SizeMe qw(total_size);
+use Data::Dumper;
+
+my $URL;
+
+
+GetOptions (
+	"url=s" => \$URL,
+) or die("Wrong args!\n" );
+
+
+
+my $ref = {
+	"HTTP::Tiny" => {},
+	"LWP::UserAgent" => {cookie_jar => undef, ssl_opts => { verify_hostname => 1 }},
+	#"WWW::Curl" => {},
+	"LWP::Curl" => {},
+	"WWW::Mechanize" => {stack_depth => 0, cookie_jar => undef, ssl_opts => { verify_hostname => 1 }},
+	
+};
+
+
+sub print_sizes
+{
+	my $ref = shift();
+	my $bytes = total_size($ref);
+	print "Size in kbytes: " . $bytes/1024 . "\n";
+	system("ps aux|grep $$|grep -v grep");
+	return;
+}
+
+sub run
+{
+	my ($module, $args_ref )= shift();
+	my $ua;
+	my $response;
+	my $i = 1000;
+	my $sec = time();
+
+	print "Loading $module\n";
+	eval "use $module";
+	die "dying: $@" if $@;
+
+	
+	$ua = $module->new(%{$args_ref});
+
+	print "Before $i iterations: ";
+	print_sizes($ua);
+
+	while($i--) {
+
+		$response = $ua->get($URL);
+
+	}
+
+	print "After iterations: ";
+	print_sizes($ua);
+	$sec = time() - $sec;
+	print "Ended after $sec seconds\n";
+	exit;
+}
+
+##############################  MAIN  ########################################
+
+
+foreach my $key (keys %{$ref} ){
+
+	my $i = fork();
+	if($i == 0){
+		run($key, $ref->{$key});
+	}
+
+	waitpid($i,0);
+
+}
+
+
+
+
